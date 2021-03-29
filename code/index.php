ba242e3d0472acb11dd56d2ba4078b5cf2ad2bc7
SeanWei <weihaoran@light2cloud.com>
2021-03-29 15:16:28 +0800

    two

---
+++ code/index.php
@@ -0,0 +1,9 @@
+<?php
+$GLOBALS['C']['AdminDir']='admin';
+$GLOBALS['C']['Debug']=0;
+
+
+
+
+require('class/cms/cms.php');
+ClassCMS_init();
---
+++ scripts/after_install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp -rf /tmp/www/. /var/www/html/
+rm -rf /tmp/www/*
